c4839c6ac64e17317d210b202f44dfefd58b8c4c
Pierre Schuvey <pierre.schuveye2n@gmail.com>
2017-09-29 11:27:09 +0200

    Fin de l'exercice 1 PHP de la prairie

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+  $chiffre1 = 10;
+  $chiffre2 = 20;
+  $addition = $chiffre1 + $chiffre2;
+  $multiplication = $chiffre1 * $chiffre2;
+
+  echo "$chiffre1 + $chiffre2 = $addition <br />";
+    echo "$chiffre1 x $chiffre2 = $multiplication";
+?>
